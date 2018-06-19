961a23276d2f7836162d0adfee655341a6472bc7
YongBeomKim <saltman21@naver.com>
2018-06-19 16:01:58 +0900

    googlefinance0.4.8

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 # coding: utf-8
+import os
+import re
+import sys
 
+from codecs import open
 from setuptools import setup, find_packages
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
 
 setup(
     name             = 'googlefinance.get',
-    version          = '0.4.7',
+    version          = '0.4.8',
     packages         = find_packages(),   # ['googlefinance.get'],  # same as the name above
     description      = 'googlefinance.get is crawling the financial data from Google finance',
+    long_description = readme,
+    long_description_content_type = 'text/x-rst',
     license          = 'MIT',
     author           = 'Yong Beom Kim',
     author_email     = 'saltman21@naver.com',
     url              = 'https://github.com/YongBeomKim/googlefinance.get',
-    download_url     = 'https://github.com/YongBeomKim/googlefinance.get/blob/master/dist/googlefinance.get-0.4.7-py3-none-any.whl',
+    download_url     = 'https://github.com/YongBeomKim/googlefinance.get/blob/master/dist/googlefinance.get-0.4.8-py3-none-any.whl',
     keywords         = 'googlefinance',
     install_requires = ['requests',
                         'pandas'],
@@ -19,5 +28,3 @@ setup(
                         'Intended Audience :: Financial and Insurance Industry',
                         'License :: OSI Approved :: MIT License']
     )
-
-
